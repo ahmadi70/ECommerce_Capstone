eb9e96336c029b2d5853678b572558c9446aee25
Nahid Ahmadi <nahidahmadi760@gmail.com>
2025-03-18 19:24:42 +0100

    Scaffolding the app

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+const App = () => {
+
+  return (
+    <div className="categories-container">
+      <div className="category-container">
+        {/* { <img /> } */}
+        <div className="category-body-container">
+          <h2>Title</h2>
+          <p>Description</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
