a060576218ca52f01ca9ad9560416d2ac02dedb4
Nahid Ahmadi <nahidahmadi760@gmail.com>
2025-03-20 03:22:16 +0100

    create navigation component

---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,23 @@
+import { Outlet, Link } from 'react-router'
+import CrownLogo from '../../assets/crown.svg'
+import './navigation.styles.css'
+
+const Navigation = () => {
+  return (
+    <>
+      <div className='navigation'>
+        <Link className='logo-container' to='/'>
+          <img src={CrownLogo} />
+        </Link>
+        <nav className='nav-links-container'>
+          <Link className='nav-link' to='/shop'>
+            SHOP
+          </Link>
+        </nav>
+      </div>
+      <Outlet />
+    </>
+  )
+}
+
+export default Navigation
