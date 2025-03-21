02e5c80f267fe937cf1598377e1e6994db100463
Nahid Ahmadi <nahidahmadi760@gmail.com>
2025-03-22 00:17:43 +0100

    define route for sign-in

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
 import { Routes, Route, Outlet } from 'react-router'
-import Navigation from './routes/navigation/navigation.component'
 import Home from './routes/home/home.component'
-
-
+import Navigation from './routes/navigation/navigation.component'
+import SignIn from './routes/sign-in/sign-in.ccomponent'
 
 const Shop = () => {
   return <h1>I am the shop page</h1>
@@ -14,6 +13,7 @@ const App = () => {
       <Route path='/' element={<Navigation />}>
         <Route index element={<Home />} />
         <Route path='shop' element={<Shop />} />
+        <Route path='sign-in' element={<SignIn />} />
       </Route>
     </Routes>
   ) 
